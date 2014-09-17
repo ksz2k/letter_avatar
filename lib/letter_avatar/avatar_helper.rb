f29413ccffb152adb5c7bc24361093d092a138a9
Krzysiek Szczuka <kszczuka@gmail.com>
2014-09-17 12:32:04 +0200

    letter_avatar_*

--- lib/letter_avatar/avatar_helper.rb
+++ lib/letter_avatar/avatar_helper.rb
@@ -1,15 +1,15 @@
 # encoding: UTF-8
 module LetterAvatar::AvatarHelper
 
-  def avatar_for(name, size = 64)
+  def letter_avatar_for(name, size = 64)
     LetterAvatar.generate(name, size)
   end
 
-  def avatar_url_for(avatar_path)
+  def letter_avatar_url_for(avatar_path)
     avatar_path.to_s.gsub(/public/,'')
   end
-  
-  def avatar_tag(name, size = 64)
+
+  def letter_avatar_tag(name, size = 64)
     image_tag(avatar_url_for(avatar_for(name, size)))
   end
 
