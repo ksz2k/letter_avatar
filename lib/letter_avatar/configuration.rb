b5479e170d16d162162ca2a1c36f83ca3859deb1
Krzysiek Szczuka <kszczuka@gmail.com>
2016-02-17 10:06:46 +0100

    Thread.current usage revoked from configuration

--- lib/letter_avatar/configuration.rb
+++ lib/letter_avatar/configuration.rb
@@ -4,35 +4,35 @@ module LetterAvatar
   module Configuration
 
     def cache_base_path
-      Thread.current[:cache_base_path]
+      @cache_base_path ||= ''
     end
 
     def cache_base_path=(v)
-      Thread.current[:cache_base_path] = v
+      @cache_base_path = v
     end
 
     def colors_palette
-      Thread.current[:colors_palette] ||= :google
+      @colors_palette ||= :google
     end
 
     def colors_palette=(v)
-      Thread.current[:colors_palette] = v if v.in?(Colors::PALETTES)
+      @colors_palette = v if v.in?(Colors::PALETTES)
     end
 
     def weight
-      Thread.current[:weight] ||= 300
+      @weight ||= 300
     end
 
     def weight=(v)
-      Thread.current[:weight] = v
+      @weight = v
     end
 
     def annotate_position
-      Thread.current[:annotate_position] ||= '-0+5'
+      @annotate_position ||= '-0+5'
     end
 
     def annotate_position=(v)
-      Thread.current[:annotate_position] = v
+      @annotate_position = v
     end
 
   end
